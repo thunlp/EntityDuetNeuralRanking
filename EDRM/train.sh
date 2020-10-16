8bf84d82456120ce845ddabbddcf32468981b9f0
EdwardZH <402151242@qq.com>
2020-10-16 04:40:36 +0000

    Update train.sh

--- EDRM/train.sh
+++ EDRM/train.sh
@@ -2,5 +2,5 @@ python train.py -data ../data/data.pt \
 -train ../data/train_ent_des_expansion \
 -valid ../data/valid_ent_des_expansion \
 -test_data ../data/test.pt \
--embed ../data/title.emb \
+-embed ../data/wordemb.txt \
 -save_model ./best_model
